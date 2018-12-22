ab83d9149279c7de7a9f5cb29bf3649309f2f9b4
Martin Slot <slot@sigsys.dk>
2018-12-22 22:10:59 +0100

    Added test feed

---
+++ src/Microservice.Extensions.NugetFeedTest/Microservice.Extensions.NugetFeedTest/Test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Microservice.Extensions.NugetFeedTest
+{
+    public class Test
+    {
+        public string Method()
+        {
+            return "Shared Test Method";
+        }
+    }
+}
