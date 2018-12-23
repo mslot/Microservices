57c6281072b772c3c698312d0028fd7bb9da4b5f
Martin Slot <slot@sigsys.dk>
2018-12-23 13:03:53 +0100

    Added new method

--- src/Microservice.Extensions.NugetFeedTest/Microservice.Extensions.NugetFeedTest/Test.cs
+++ src/Microservice.Extensions.NugetFeedTest/Microservice.Extensions.NugetFeedTest/Test.cs
@@ -4,6 +4,11 @@ namespace Microservice.Extensions.NugetFeedTest
 {
     public class Test
     {
+        public string MethodNew()
+        {
+            return "new method";
+        }
+
         public string Method()
         {
             string s = "Shared Test Method";
