b9a486458dc2f04b0abc369b5bb9b75cacc07dbb
Martin Slot <slot@sigsys.dk>
2018-12-29 21:33:29 +0100

    Added controller and vault integration

---
+++ src/WebAPI/WebAPI/Controllers/SecretsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SecretsController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public SecretsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { _configuration["ARMSecret"] };
+        }
+    }
+}
