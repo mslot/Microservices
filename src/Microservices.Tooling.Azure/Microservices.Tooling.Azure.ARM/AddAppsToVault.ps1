46485a151bc17d67d0260c3692362d4df80f6fe4
Martin Slot <slot@sigsys.dk>
2018-12-30 10:32:37 +0100

    Added az login to script

--- src/Microservices.Tooling.Azure/Microservices.Tooling.Azure.ARM/AddAppsToVault.ps1
+++ src/Microservices.Tooling.Azure/Microservices.Tooling.Azure.ARM/AddAppsToVault.ps1
@@ -1,3 +1,4 @@
-﻿$json=az webapp identity assign --name "microservices-webapi-$env:ENVIRONMENT" --resource-group "microservices-$env:ENVIRONMENT-rg"
+﻿az login
+$json=az webapp identity assign --name "microservices-webapi-$env:ENVIRONMENT" --resource-group "microservices-$env:ENVIRONMENT-rg"
 $parsedObject=$json | ConvertFrom-Json
 az keyvault set-policy --name '$env:KEYVAULTNAME-$env:ENVIRONMENT' --object-id $parsedObject.principalId --secret-permissions get list
