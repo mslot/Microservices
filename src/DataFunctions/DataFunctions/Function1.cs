41fefc71e4121d44cf71cbf95c56309f6aaf8460
Martin Slot <slot@sigsys.dk>
2018-12-30 21:55:29 +0100

    Removed usings not used

--- src/DataFunctions/DataFunctions/Function1.cs
+++ src/DataFunctions/DataFunctions/Function1.cs
@@ -1,12 +1,9 @@
-using System;
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Azure.KeyVault;
