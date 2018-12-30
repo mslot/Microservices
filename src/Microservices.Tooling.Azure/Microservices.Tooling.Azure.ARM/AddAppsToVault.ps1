f90c927a97215ab78863a38942eb4ab60e81642c
Martin Slot <slot@sigsys.dk>
2018-12-30 11:35:47 +0100

    Updated to use powershell instead of az

--- src/Microservices.Tooling.Azure/Microservices.Tooling.Azure.ARM/AddAppsToVault.ps1
+++ src/Microservices.Tooling.Azure/Microservices.Tooling.Azure.ARM/AddAppsToVault.ps1
@@ -1,4 +1,8 @@
-﻿az login
-$json=az webapp identity assign --name "microservices-webapi-$env:ENVIRONMENT" --resource-group "microservices-$env:ENVIRONMENT-rg"
-$parsedObject=$json | ConvertFrom-Json
-az keyvault set-policy --name '$env:KEYVAULTNAME-$env:ENVIRONMENT' --object-id $parsedObject.principalId --secret-permissions get list
+﻿$webapp=Set-AzWebApp -AssignIdentity $true -Name microservices-webapi-$env:ENVIRONMENT -ResourceGroupName microservices-$env:ENVIRONMENT-rg
+
+Set-AzureRmKeyVaultAccessPolicy -VaultName $env:KEYVAULTNAME-$env:ENVIRONMENT -ServicePrincipalName $webapp.Identity.PrincipalId -PermissionsToKeys get,list
+
+
+#$json=az webapp identity assign --name "microservices-webapi-$env:ENVIRONMENT" --resource-group "microservices-$env:ENVIRONMENT-rg"
+#$parsedObject=$json | ConvertFrom-Json
+#az keyvault set-policy --name '$env:KEYVAULTNAME-$env:ENVIRONMENT' --object-id $parsedObject.principalId --secret-permissions get list
