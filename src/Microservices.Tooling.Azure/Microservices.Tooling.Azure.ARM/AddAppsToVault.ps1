8414c0b54933badd1aafee931970166757709b14
Martin Slot <slot@sigsys.dk>
2018-12-30 13:23:00 +0100

    Added debug

--- src/Microservices.Tooling.Azure/Microservices.Tooling.Azure.ARM/AddAppsToVault.ps1
+++ src/Microservices.Tooling.Azure/Microservices.Tooling.Azure.ARM/AddAppsToVault.ps1
@@ -1,5 +1,10 @@
 ﻿$webapp=Set-AzureRmWebApp -AssignIdentity $true -Name microservices-webapi-$env:ENVIRONMENT -ResourceGroupName microservices-$env:ENVIRONMENT-rg
 
+Write-Host microservices-$env:ENVIRONMENT-rg
+Write-Host microservices-webapi-$env:ENVIRONMENT
+Write-Host env:KEYVAULTNAME-$env:ENVIRONMENT
+Write-Host $webapp.Identity.PrincipalId
+
 Set-AzureRmKeyVaultAccessPolicy -VaultName $env:KEYVAULTNAME-$env:ENVIRONMENT -ObjectId $webapp.Identity.PrincipalId -PermissionsToKeys get,list -PermissionsToSecrets get,list
 
 #This is the same as above, just with the az command
