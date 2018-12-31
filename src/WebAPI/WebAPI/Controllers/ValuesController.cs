5d490dc0b7ad7327a09f73a32817aae88b7a4a38
Martin Slot <slot@sigsys.dk>
2018-12-31 16:33:19 +0100

    value14

--- src/WebAPI/WebAPI/Controllers/ValuesController.cs
+++ src/WebAPI/WebAPI/Controllers/ValuesController.cs
@@ -15,7 +15,7 @@ namespace WebAPI.Controllers
         public ActionResult<IEnumerable<string>> Get()
         {
             return new string[] { "value1", "value2", "value3", "value4" , "value5" ,
-                "value6" , "value7" , "value8" , "value9" , "value10" , "value11" , "value12" , "value13" };
+                "value6" , "value7" , "value8" , "value9" , "value10" , "value11" , "value12" , "value13" , "value14" };
         }
 
         // GET api/values/5
