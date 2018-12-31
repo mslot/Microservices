56732e7ab709bfd7502ea7f1e2b2c0183beacfcd
Martin Slot <slot@sigsys.dk>
2018-12-31 21:48:28 +0100

    value20

--- src/WebAPI/WebAPI/Controllers/ValuesController.cs
+++ src/WebAPI/WebAPI/Controllers/ValuesController.cs
@@ -16,7 +16,7 @@ namespace WebAPI.Controllers
         {
             return new string[] { "value1", "value2", "value3", "value4" , "value5" ,
                 "value6" , "value7" , "value8" , "value9" , "value10" , "value11" , "value12" , "value13" ,
-                "value14" , "value15" , "value16" , "value17" , "value18" , "value19" };
+                "value14" , "value15" , "value16" , "value17" , "value18" , "value19" , "value20" };
         }
 
         // GET api/values/5
