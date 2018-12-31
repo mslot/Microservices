58bc2ace817111402c732691793071566cbe045b
Martin Slot <slot@sigsys.dk>
2018-12-31 14:15:38 +0100

    Added final value

--- src/WebAPI/WebAPI/Controllers/ValuesController.cs
+++ src/WebAPI/WebAPI/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace WebAPI.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2", "value3", "value4" , "value5" , "value6" };
+            return new string[] { "value1", "value2", "value3", "value4" , "value5" , "value6" , "value7" };
         }
 
         // GET api/values/5
