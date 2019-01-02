2b37f2df5903de6cca4965f622b1e1ff66bb8352
Martin Slot <slot@sigsys.dk>
2019-01-02 02:09:38 +0100

    Added fix for current directory

--- src/DataFunctions/DataFunctions/Function2.cs
+++ src/DataFunctions/DataFunctions/Function2.cs
@@ -12,13 +12,23 @@ namespace DataFunctions
     public static class Function2
     {
         private static IConfigurationBuilder _builder = new ConfigurationBuilder()
-                                .SetBasePath(Environment.CurrentDirectory)
+                                .SetBasePath(GetWorkingDir())
                                 .AddJsonFile("settings.json", optional: false, reloadOnChange: false)
                                 .AddJsonFile("local.settings.json", optional: true, reloadOnChange: false)
                                 .AddEnvironmentVariables()
                                 .AddUserSecrets<StartUp>();
         private static IConfigurationRoot _config;
 
+        private static string GetWorkingDir()
+        {
+            string current = Environment.CurrentDirectory;
+            string dir = current;
+
+            if (current.StartsWith(@"D:\windows")) //this is a hack to get things working on Azure. I need to figure out a better way of doing this.
+                dir = @"D:\home\site\wwwroot";
+
+            return dir;
+        }
 
         static Function2()
         {
@@ -46,7 +56,7 @@ namespace DataFunctions
         }
 
         [FunctionName("Function2")]
-        public static void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
+        public static void Run([TimerTrigger("*/5 * * * * *")]TimerInfo myTimer, ILogger log)
         {
             _config.Reload(); //We somehow need to reload config so we can get changes in Vault
             log.LogInformation($"RuntimeTest: {_config["RuntimeTest"]}");
